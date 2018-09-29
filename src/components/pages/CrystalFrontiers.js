19fb42b4276458a1bde04d2af04af51890d576f3
Theu <info@matteocontini.com>
2018-09-29 21:57:57 +0200

    added basic routing

---
+++ src/components/pages/CrystalFrontiers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const CrystalFrontiers = () => {
+    return (
+        <div>
+            CrystalFrontiers
+        </div>
+    );
+}
+
+export default CrystalFrontiers;
---
+++ src/components/pages/NewYork.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NewYork = () => {
+    return (
+        <div>
+        NewYork
+        </div>
+    );
+}
+
+export default NewYork;
