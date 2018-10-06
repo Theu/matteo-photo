80e63269902eed62184e40e1c70eba5cec9894cb
Theu <info@matteocontini.com>
2018-10-06 12:02:39 +0200

    created first slider draft

---
+++ src/components/slider/Slide.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './slide.css';
+
+// HI, I am a functional Component
+const Slide = ({ image }) => { 
+    const styles = {
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '50% 60%'
+    }
+    return <div className='slide' style={styles}></div>
+};
+
+export default Slide;
