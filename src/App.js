707936be2dbb97d514102338c2de4dd341dc2782
Theu <info@matteocontini.com>
2019-06-03 21:59:57 +0200

    a little more style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { PureComponent } from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-  } from 'react-router-dom';
 
 import './index.css'
 
 import Homepage from './Homepage';
-
-import CrystalFrontiers from './components/pages/CrystalFrontiers'
 
 class App extends PureComponent {
 
