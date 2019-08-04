e522d88c2a981ef8fa302398c5ab37d63ec6bdc9
Theu <info@matteocontini.com>
2019-08-04 20:05:36 +0200

    renaming

--- src/images/crystalFrontier/index.js
+++ src/images/crystalFrontier/index.js
@@ -1,112 +1,112 @@
-import img_01_CF_HP from '../../assets/img/hp/crystalFrontier/01_72.jpg';
-import img_02_CF_HP from '../../assets/img/hp/crystalFrontier/02_72.jpg';
-import img_03_CF_HP from '../../assets/img/hp/crystalFrontier/03_72.jpg';
-import img_04_CF_HP from '../../assets/img/hp/crystalFrontier/04_72.jpg';
-import img_05_CF_HP from '../../assets/img/hp/crystalFrontier/05_72.jpg';
-import img_06_CF_HP from '../../assets/img/hp/crystalFrontier/06_72.jpg';
-import img_07_CF_HP from '../../assets/img/hp/crystalFrontier/07_72.jpg';
-import img_08_CF_HP from '../../assets/img/hp/crystalFrontier/08_72.jpg';
-import img_09_CF_HP from '../../assets/img/hp/crystalFrontier/09_72.jpg';
-import img_10_CF_HP from '../../assets/img/hp/crystalFrontier/10_72.jpg';
-import img_11_CF_HP from '../../assets/img/hp/crystalFrontier/11_72.jpg';
-import img_12_CF_HP from '../../assets/img/hp/crystalFrontier/12_72.jpg';
+import CF_01_CF_HP from '../../assets/img/hp/crystalFrontier/01_72.jpg';
+import CF_02_CF_HP from '../../assets/img/hp/crystalFrontier/02_72.jpg';
+import CF_03_CF_HP from '../../assets/img/hp/crystalFrontier/03_72.jpg';
+import CF_04_CF_HP from '../../assets/img/hp/crystalFrontier/04_72.jpg';
+import CF_05_CF_HP from '../../assets/img/hp/crystalFrontier/05_72.jpg';
+import CF_06_CF_HP from '../../assets/img/hp/crystalFrontier/06_72.jpg';
+import CF_07_CF_HP from '../../assets/img/hp/crystalFrontier/07_72.jpg';
+import CF_08_CF_HP from '../../assets/img/hp/crystalFrontier/08_72.jpg';
+import CF_09_CF_HP from '../../assets/img/hp/crystalFrontier/09_72.jpg';
+import CF_10_CF_HP from '../../assets/img/hp/crystalFrontier/10_72.jpg';
+import CF_11_CF_HP from '../../assets/img/hp/crystalFrontier/11_72.jpg';
+import CF_12_CF_HP from '../../assets/img/hp/crystalFrontier/12_72.jpg';
 
-import img_01_CF_GL from '../../assets/img/gallery/crystalFrontier/01_CF.jpg';
-import img_02_CF_GL from '../../assets/img/gallery/crystalFrontier/02_CF.jpg';
-import img_03_CF_GL from '../../assets/img/gallery/crystalFrontier/03_CF.jpg';
-import img_04_CF_GL from '../../assets/img/gallery/crystalFrontier/04_CF.jpg';
-import img_05_CF_GL from '../../assets/img/gallery/crystalFrontier/05_CF.jpg';
-import img_06_CF_GL from '../../assets/img/gallery/crystalFrontier/06_CF.jpg';
-import img_07_CF_GL from '../../assets/img/gallery/crystalFrontier/07_CF.jpg';
-import img_08_CF_GL from '../../assets/img/gallery/crystalFrontier/08_CF.jpg';
-import img_09_CF_GL from '../../assets/img/gallery/crystalFrontier/09_CF.jpg';
-import img_10_CF_GL from '../../assets/img/gallery/crystalFrontier/10_CF.jpg';
-import img_11_CF_GL from '../../assets/img/gallery/crystalFrontier/11_CF.jpg';
-import img_12_CF_GL from '../../assets/img/gallery/crystalFrontier/12_CF.jpg';
+import CF_01_CF_GL from '../../assets/img/gallery/crystalFrontier/01_CF.jpg';
+import CF_02_CF_GL from '../../assets/img/gallery/crystalFrontier/02_CF.jpg';
+import CF_03_CF_GL from '../../assets/img/gallery/crystalFrontier/03_CF.jpg';
+import CF_04_CF_GL from '../../assets/img/gallery/crystalFrontier/04_CF.jpg';
+import CF_05_CF_GL from '../../assets/img/gallery/crystalFrontier/05_CF.jpg';
+import CF_06_CF_GL from '../../assets/img/gallery/crystalFrontier/06_CF.jpg';
+import CF_07_CF_GL from '../../assets/img/gallery/crystalFrontier/07_CF.jpg';
+import CF_08_CF_GL from '../../assets/img/gallery/crystalFrontier/08_CF.jpg';
+import CF_09_CF_GL from '../../assets/img/gallery/crystalFrontier/09_CF.jpg';
+import CF_10_CF_GL from '../../assets/img/gallery/crystalFrontier/10_CF.jpg';
+import CF_11_CF_GL from '../../assets/img/gallery/crystalFrontier/11_CF.jpg';
+import CF_12_CF_GL from '../../assets/img/gallery/crystalFrontier/12_CF.jpg';
 
 const crystalFrontiers = [
     {
         title: '01',
-        homeGridSrc: img_01_CF_HP,
-        gallerySrc: img_01_CF_GL,
-        id: 'img_01_72',
+        homeGridSrc: CF_01_CF_HP,
+        gallerySrc: CF_01_CF_GL,
+        id: 'CF_01_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '02',
-        homeGridSrc: img_02_CF_HP,
-        gallerySrc: img_02_CF_GL,
-        id: 'img_02_72',
+        homeGridSrc: CF_02_CF_HP,
+        gallerySrc: CF_02_CF_GL,
+        id: 'CF_02_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '03',
-        homeGridSrc: img_03_CF_HP,
-        gallerySrc: img_03_CF_GL,
-        id: 'img_03_72',
+        homeGridSrc: CF_03_CF_HP,
+        gallerySrc: CF_03_CF_GL,
+        id: 'CF_03_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '04',
-        homeGridSrc: img_04_CF_HP,
-        gallerySrc: img_04_CF_GL,
-        id: 'img_04_72',
+        homeGridSrc: CF_04_CF_HP,
+        gallerySrc: CF_04_CF_GL,
+        id: 'CF_04_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '05',
-        homeGridSrc: img_05_CF_HP,
-        gallerySrc: img_05_CF_GL,
-        id: 'img_05_72',
+        homeGridSrc: CF_05_CF_HP,
+        gallerySrc: CF_05_CF_GL,
+        id: 'CF_05_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '06',
-        homeGridSrc: img_06_CF_HP,
-        gallerySrc: img_06_CF_GL,
-        id: 'img_06_72',
+        homeGridSrc: CF_06_CF_HP,
+        gallerySrc: CF_06_CF_GL,
+        id: 'CF_06_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '07',
-        homeGridSrc: img_07_CF_HP,
-        gallerySrc: img_07_CF_GL,
-        id: 'img_07_72',
+        homeGridSrc: CF_07_CF_HP,
+        gallerySrc: CF_07_CF_GL,
+        id: 'CF_07_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '08',
-        homeGridSrc: img_08_CF_HP,
-        gallerySrc: img_08_CF_GL,
-        id: 'img_08_72',
+        homeGridSrc: CF_08_CF_HP,
+        gallerySrc: CF_08_CF_GL,
+        id: 'CF_08_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '09',
-        homeGridSrc: img_09_CF_HP,
-        gallerySrc: img_09_CF_GL,
-        id: 'img_09_72',
+        homeGridSrc: CF_09_CF_HP,
+        gallerySrc: CF_09_CF_GL,
+        id: 'CF_09_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '10',
-        homeGridSrc: img_10_CF_HP,
-        gallerySrc: img_10_CF_GL,
-        id: 'img_10_72',
+        homeGridSrc: CF_10_CF_HP,
+        gallerySrc: CF_10_CF_GL,
+        id: 'CF_10_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '11',
-        homeGridSrc: img_11_CF_HP,
-        gallerySrc: img_11_CF_GL,
-        id: 'img_11_72',
+        homeGridSrc: CF_11_CF_HP,
+        gallerySrc: CF_11_CF_GL,
+        id: 'CF_11_72',
         serie: 'crystal-frontiers'
     },
     {
         title: '12',
-        homeGridSrc: img_12_CF_HP,
-        gallerySrc: img_12_CF_GL,
-        id: 'img_12_72',
+        homeGridSrc: CF_12_CF_HP,
+        gallerySrc: CF_12_CF_GL,
+        id: 'CF_12_72',
         serie: 'crystal-frontiers'
     }
 ];
